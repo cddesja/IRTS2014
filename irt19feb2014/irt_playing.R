50788f41ea1ccd76f6329dc249a7661cd4a1bfa2
Christopher David Desjardins <cddesjardins@gmail.com>
2014-02-19 12:23:28 +0000

    Updated IRT presentation script

--- irt19feb2014/irt_playing.R
+++ irt19feb2014/irt_playing.R
@@ -22,3 +22,22 @@ negdis <- pl.two(thetas,a=-1.5,d=0)
 pdf(file = "~/Documents/HI/Teaching/IRTworkshop/irt19feb2014/fig2.pdf")	
 plot(negdis~thetas,type = "l", col = "black",ylab = "Probability",xlab = "Theta")
 dev.off()
+
+pl.three <- function(theta,a,d,g){
+	g + (1 -g)*exp(a*(theta - d))/(1 + exp(a*(theta - d)))
+	} 
+thetas <- seq(from = -3, to = 3, by = .1)
+g = c(0,.25,.5)
+d = c(0,0,0)
+a = c(1,1,1)
+
+irfs <- matrix(nrow = 61, ncol=3)
+for(i in 1:3){
+	irfs[,i] <- pl.three(theta=thetas,d=d[i],a=a[i],g = g[i])
+	}
+
+pdf(file = "~/Documents/HI/Teaching/IRTworkshop/irt19feb2014/fig3.pdf")	
+plot(irfs[,1]~thetas,type = "l", col = "purple",ylab = "Probability",xlab = "Theta")
+lines(irfs[,2]~thetas,type = "l", col = "blue")
+lines(irfs[,3]~thetas,type = "l", col = "black")
+dev.off()
